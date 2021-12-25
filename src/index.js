32bcdccf82faf815cbdfc583b77c1670a799c324
Kiran Adhikari <kiranadh1452@gmail.com>
2021-12-25 17:15:32 -0500

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+let currentIndex = 0;
+
+let interval;
+let dx = 0;
+
+
 const wrapper = document.getElementById("wrapper");
 const images = document.getElementById("images");
 
 const imageWidth = 500;
 const imageCount = images.children.length;
+
+const requiredTime = 10;
+let animateSpeed = (imageWidth / requiredTime) /10;
 
 images.style.width = `${imageCount * imageWidth}px`;
 
@@ -15,33 +24,38 @@ for (let i = 0; i < imageCount; i++) {
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
-let currentIndex = 0;
 
-let interval;
-let dx = 0;
-nextBtn.onclick = function () {
-  currentIndex++;
-  if(currentIndex === imageCount){
-    currentIndex=0;
-    images.style.left = `0px`;
-    return 0;
+nextBtn.onclick = function(){
+  right_slide();
+}
+
+let startAnimation;
+
+function right_slide() {
+  startAnimation = window.requestAnimationFrame(right_slide);
+
+  if(dx >= imageWidth){
+    window.cancelAnimationFrame(startAnimation);
+    dx = 0;
+    currentIndex = (currentIndex+1)%imageCount;
   }
+  else{
+    dx += animateSpeed;
+    dx = Math.min(dx , imageWidth);
+  }
+  images.style.left = `-${(imageWidth*(currentIndex))+dx}px`;
+}
 
-  interval = setInterval(() => {
-    dx++;
-    images.style.left = `-${(imageWidth*(currentIndex-1))+dx}px`;
+// function highlightCurrentBox(){
 
-    if (dx >= imageWidth){
-      clearInterval(interval);
-      dx = 0;
-    }
-  }, 1);
-};
+// }
 
 prevBtn.onclick = function () {
   dx = imageWidth;
   if(currentIndex === 0){
-    currentIndex = imageCount -1 ;
+    currentIndex = imageCount-1 ;
+    images.style.left = `-${(currentIndex * imageWidth)}px`;
+    return 0;
   }
   else{
     currentIndex--;
