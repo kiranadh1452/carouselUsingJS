085978027601b94d1913eed7d0da8c44b648c8c0
Kiran Adhikari <kiranadh1452@gmail.com>
2021-12-25 21:06:26 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ let currentIndex = 0;
 let interval;
 let dx = 0;
 
-
 const wrapper = document.getElementById("wrapper");
 const images = document.getElementById("images");
 
 const imageWidth = 500;
 const imageCount = images.children.length;
 
-const requiredTime = 10; //what should the tansition time be in seconds
+const requiredTime = 5; //what should the tansition time be in seconds
 let animateSpeed = (imageWidth / requiredTime) /10;
 
 images.style.width = `${imageCount * imageWidth}px`;
@@ -41,6 +40,7 @@ arrowBtn1.onclick = function(){
   currentIndex--;
   if (currentIndex<0) currentIndex = imageCount-1;
   left_slide();
+  updateDots();
 }
 
 function left_slide() {
@@ -65,6 +65,7 @@ arrowBtn2.onclick = function(){
     currentIndex = 0;
   }
   right_slide();
+  updateDots();
 }
 
 function right_slide() {
@@ -80,3 +81,29 @@ function right_slide() {
   images.style.left = `-${(imageWidth*(currentIndex-1))+dx}px`;
 }
 
+let dotsContainer = document.createElement('div');
+wrapper.appendChild(dotsContainer);
+
+dotsContainer.classList.add('dots-container');
+let dotSpecific = [];
+for (let count = 0; count < imageCount; count++) {
+  dotSpecific[count] = document.createElement('button');
+  dotSpecific[count].classList.add('dot-specific');
+  dotsContainer.appendChild(dotSpecific[count]);
+  dotSpecific[count].addEventListener('click', function (){
+    images.style.left = `-${(imageWidth*(count))}px`;
+    currentIndex = count;
+    for(let i=0; i< imageCount; i++){
+      if(i == count) dotSpecific[i].classList.add('dot-current');
+      else dotSpecific[i].classList.remove('dot-current');
+    }
+  });
+}
+dotSpecific[0].classList.add('dot-current');
+
+function updateDots(){
+  for(let i=0; i< imageCount; i++){
+    if(i == currentIndex) dotSpecific[i].classList.add('dot-current');
+    else dotSpecific[i].classList.remove('dot-current');
+  }
+}
